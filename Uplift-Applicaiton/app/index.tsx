8d71c21fe23bca0abb03c638a220a8f6d3b1162a
jav0402 <111144138+jav0402@users.noreply.github.com>
2024-07-24 18:02:51 +0800

    imp int screen

--- Uplift-Applicaiton/app/index.tsx
+++ Uplift-Applicaiton/app/index.tsx
@@ -1,6 +1,13 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
 import React from 'react'
+import {LinearGradient} from "expo-linear-gradient"
+import { StatusBar } from 'expo-status-bar'
+
+
+
 import beachimg from "@/assets/meditation-images/beach.webp"
+import CustomButton from '@/componets/CustomButton'
+
 const index = () => {
   return (
     <View className='flex-1'>
@@ -9,7 +16,32 @@ const index = () => {
         resizeMode='cover'
         className='flex-1'
       >
-        <Text>app</Text>
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+            
+            <SafeAreaView>
+              <View>
+                <Text className="text-center text-white font-bold text-4xl"> 
+                  UpLift
+                </Text>
+                
+                <Text className="text-center text-white text-regular text-2xl mt-3"> 
+                  discription of application
+                </Text>
+              </View>
+
+              <View>
+                <CustomButton onPress={() => console.log("button pressed")} 
+                title="get started"
+                />
+              </View>
+
+              <StatusBar style="light"/>
+
+            </SafeAreaView>
+        </LinearGradient>
+        {/* <Text>app</Text> */}
 
       </ImageBackground>
     </View>
