cda099ccffdf55a4c69da0d9755e445ef81765cc
jav0402 <jav0402@gmail.com>
2024-07-25 22:57:02 +0800

    stack.screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Slot } from 'expo-router'
+import { Slot , Stack} from 'expo-router'
 
 const RootLayout = () => {
-  // return (
-  //   <View style = {styles.container}>
-  //     <Text>RootLayout</Text>
-  //   </View>
-  // )
-  return <Slot />
+ /*  return (
+    <View style = {styles.container}>
+      <Text>RootLayout</Text>
+    </View>
+  ) */
+/*   return (
+    <>
+      <Text>Header</Text>
+        <Slot />
+      <Text>footer</Text>
+    </>
+  ) */
+  return(
+    <Stack>
+      <Stack.Screen name = "index"options={{ headerShown: false}} />
+    </Stack>
+  )
 }
 
 export default RootLayout;
