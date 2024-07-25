64a6306d6d37c8d84c49703fc66ad541f59d9742
jav0402 <jav0402@gmail.com>
2024-07-26 00:21:45 +0800

    add fonts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Slot , Stack} from 'expo-router'
+import { useFonts } from 'expo-font'
 
 const RootLayout = () => {
  /*  return (
@@ -15,6 +16,11 @@ const RootLayout = () => {
       <Text>footer</Text>
     </>
   ) */
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black":require("../assets/fonts/Poppins-Black.ttf"),
+    
+  })
+
   return(
     <Stack>
       <Stack.Screen name = "index"options={{ headerShown: false}} />
