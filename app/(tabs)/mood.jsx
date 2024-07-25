2724d10da8177d52c4c26f086bc9023425d846ae
jav0402 <jav0402@gmail.com>
2024-07-26 00:39:14 +0800

    create each tabs page

---
+++ app/(tabs)/mood.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Mood = () => {
+  return (
+    <View>
+      <Text>Mood</Text>
+    </View>
+  )
+}
+
+export default Mood
