12bf2fdd6b3c95071139db36c1c2e9334daa2713
jhng035 <115973137+jhng035@users.noreply.github.com>
2024-08-24 19:41:22 +0800

    #22 included factors (affecting mood) selection, displayed after mood selection and updated overall display

--- app/(tabs)/mood.jsx
+++ app/(tabs)/mood.jsx
@@ -6,6 +6,31 @@ import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 const Mood = () => {
   // Default mood set to 'neutral'
   const [mood, setMood] = useState('neutral');
+  // Hide factor selection display by default
+  const [factors, setFactors] = useState(false);
+  
+  const [selectedFactors, setSelectedFactors] = useState([]);
+
+  /**
+   * Toggle display from mood selection to factor selection
+   * @param selectedMood - current mood selected
+   * If at mood selection display, setFactors = true and toggle to factor selection
+   * If at factor selection, toggle back to mood selection and reset any selected factors
+   */
+  const toggleDisplay = (selectedMood) => {
+    !factors ? (setMood(selectedMood), setFactors(true)) : (setFactors(false), setSelectedFactors([]))
+  }
+  const toggleFactor = (factor) => {
+    if (selectedFactors.includes(factor)) {
+      setSelectedFactors(selectedFactors.filter((item) => item !== factor));
+    } else {
+      setSelectedFactors([...selectedFactors, factor]);
+    }
+  }
+  // Placeholder function, to be replaced with data storing statements
+  const storeData = () => {
+    console.log("Mood data saved");
+  }
 
   // Moods data including colors, text, and icons
   const moods = {
@@ -36,47 +61,110 @@ const Mood = () => {
     },
   };
 
+  // Factors data, name and icon
+  const factorsList = [
+    { name: 'Work', icon: 'briefcase' },
+    { name: 'School', icon: 'book' },
+    { name: 'Love', icon: 'heart' },
+    { name: 'Friends', icon: 'user-group' },
+    { name: 'Family', icon: 'house-chimney' },
+    { name: 'Money', icon: 'money-bill' },
+    { name: 'Health', icon: 'heart-pulse' },
+    { name: 'Life', icon: 'user-large' },
+    { name: 'None', icon: 'times-circle' },
+  ];
+
   return (
     // Background set to selected mood colour
     <SafeAreaView className={`flex-1 ${moods[mood].color} justify-center items-center`}>
       <View className="flex-1 justify-evenly">
-        <View className="items-center">
-          <Text className="text-white text-4xl font-bold text-center">How are you feeling today?</Text>
-        </View>
-
-        <View className="items-center">
-          {/* Icon displaying current selected mood */}
-          <FontAwesome6 name={moods[mood].icon} size={150} color="white" />
-          {/* Text displaying current selected mood */}
-          <Text className="text-white text-2xl mt-4">I'm Feeling {moods[mood].text}</Text>
-        </View>
-
-        <View className="items-center">
-          {/* Mood selection */}
-          <View className="flex-row">
-            {Object.keys(moods).map((key) => (
-              // User interaction component 
-              <TouchableOpacity
-                key={key}
-                onPress={() => setMood(key)}
-                className="mx-2"
-                >
-                {/* Display icons for user interaction */}
-                <FontAwesome6
-                  name={moods[key].icon}
-                  size={40}
-                  // Change colour when active
-                  color={mood === key ? 'white' : 'black'}
-                />
+        {/* Default: display mood selection */}
+        {!factors ? (
+          // Mood selection display
+          <>
+            {/* Main header display*/}
+            <View className="items-center">
+              <Text className="text-white text-4xl font-bold text-center">How are you feeling today?</Text>
+            </View>
+            {/* Current selected mood icon and text display*/}
+            <View className="items-center">
+              <FontAwesome6 name={moods[mood].icon} size={150} color="white" />
+              <Text className="text-white text-2xl mt-4">I'm Feeling {moods[mood].text}</Text>
+            </View>
+            {/* Mood selection and buttons display */}
+            <View className="items-center">
+              {/* Mood selection */}
+              <View className="flex-row">
+                {Object.keys(moods).map((key) => (
+                  // User interaction component 
+                  <TouchableOpacity
+                    key={key}
+                    // Set mood as current mood
+                    onPress={() => setMood(key)}
+                    className="mx-2"
+                  >
+                    {/* Display icons for user interaction */}
+                    <FontAwesome6
+                      name={moods[key].icon}
+                      size={40}
+                      // Change colour when active
+                      color={mood === key ? 'white' : 'black'}
+                    />
+                  </TouchableOpacity>
+                ))}
+              </View>
+              {/* Set Mood button */}
+              <TouchableOpacity 
+                className="bg-white py-4 px-8 mt-8 rounded-full w-full"
+                // Switch to factor selection display
+                onPress={()=> toggleDisplay(mood)}
+              >
+                <Text className="text-black text-lg font-bold text-center">Set Mood</Text>
               </TouchableOpacity>
-            ))}
-          </View>
-
-          {/* Set mood button */}
-          <TouchableOpacity className="bg-white py-4 px-8 mt-8 rounded-full w-full">
-            <Text className="text-black text-lg font-bold text-center">Set Mood</Text>
-          </TouchableOpacity>
-        </View>
+            </View>
+          </>
+        ) : (
+          // Factor selection display
+          <>
+            {/* Main header display */}
+            <View className="items-center">
+              <Text className="text-white text-4xl font-bold text-center">What are the factors affecting your mood?</Text>
+            </View>
+            {/* Factor selection and buttons display */}
+            <View className="items-center">
+              {/* Factors selection */}
+              <View className="flex-row flex-wrap justify-around w-full">
+                {factorsList.map((factor, index) => (
+                  <TouchableOpacity
+                    key={index}
+                    // Change background colour when selected
+                    className={`p-4 m-2 rounded-lg w-24 items-center ${selectedFactors.includes(factor.name) ? 'bg-orange-300' : 'bg-white'}`}
+                    // Toggle factor selection
+                    onPress={() => toggleFactor(factor.name)}
+                  >
+                    <FontAwesome6 name={factor.icon} color='black' size={30}
+                    />
+                    <Text className="text-black mt-2 text-center">{factor.name}</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+              {/* Save Factors button */}
+              <TouchableOpacity
+                className="bg-white py-4 px-8 mt-8 rounded-full w-[90%]"
+                onPress={storeData}
+              >
+                <Text className="text-black text-lg font-bold text-center">Save Factors</Text>
+              </TouchableOpacity>
+              {/* Back button - to mood selection */}
+              <TouchableOpacity
+                className="bg-black py-4 px-8 mt-8 rounded-full w-[90%]"
+                onPress={()=> toggleDisplay(mood)}
+              >
+                <Text className="text-white text-lg font-bold text-center">Back</Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
