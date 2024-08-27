49939afd99fd8ccddd60326d78e7b3ed2f6faa82
jav0402 <jav0402@gmail.com>
2024-08-27 17:09:47 +0800

    npm i and create quiz.jsx

---
+++ app/quiz.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const quiz = () => {
+  return (
+    <View>
+      <Text>quiz</Text>
+    </View>
+  )
+}
+
+export default quiz
